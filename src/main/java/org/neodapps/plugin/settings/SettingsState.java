6e3fd740af3d5d18b24fa27d304c8fef5d9716aa
Irshad Nilam <mail@irshadnilam.com>
2021-06-25 17:23:30 +0530

    Add plugin settings panel

---
+++ src/main/java/org/neodapps/plugin/settings/SettingsState.java
@@ -0,0 +1,43 @@
+/*
+ *  Use of this source code is governed by the Apache 2.0 license that can be
+ *  found in the LICENSE file.
+ */
+
+package org.neodapps.plugin.settings;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Supports storing the application settings in a persistent way.
+ * The {@link State} and {@link Storage} annotations define the name of the data and the file name
+ * where these persistent application settings are stored.
+ */
+@State(
+    name = "org.neodapps.plugin.settings.SettingsState",
+    storages = {@Storage("NeoPluginSettings.xml")}
+)
+public class SettingsState implements PersistentStateComponent<SettingsState> {
+  public String neoExpressLocation = "";
+  public String dotNetRoot = "";
+
+  public static SettingsState getInstance() {
+    return ApplicationManager.getApplication().getService(SettingsState.class);
+  }
+
+  @Nullable
+  @Override
+  public SettingsState getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(@NotNull SettingsState state) {
+    XmlSerializerUtil.copyBean(state, this);
+  }
+}
