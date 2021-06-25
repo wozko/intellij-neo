a4525c691baf8a54de377648f634231852de6665
Irshad Nilam <mail@irshadnilam.com>
2021-06-25 16:41:25 +0530

    Add message bundle

---
+++ src/main/java/org/neodapps/plugin/MessageBundle.java
@@ -0,0 +1,38 @@
+/*
+ *  Use of this source code is governed by the Apache 2.0 license that can be
+ *  found in the LICENSE file.
+ */
+
+package org.neodapps.plugin;
+
+import com.intellij.DynamicBundle;
+import java.util.function.Supplier;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+/**
+ * Supports getting string constants for the plugin from bundle.
+ */
+public final class MessageBundle extends DynamicBundle {
+  @NonNls
+  private static final String BUNDLE = "messages.NeoBundle";
+  private static final MessageBundle INSTANCE = new MessageBundle();
+
+  private MessageBundle() {
+    super(BUNDLE);
+  }
+
+  @NotNull
+  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key,
+                                    Object @NotNull ... params) {
+    return INSTANCE.getMessage(key, params);
+  }
+
+  @NotNull
+  public static Supplier<@Nls String> messagePointer(
+      @NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getLazyMessage(key, params);
+  }
+}
